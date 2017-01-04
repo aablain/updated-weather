e4c2efaf2ded36ceda979231753b90a9b35e0599
Austin Blain and Annie Sonna <annie@>
2017-01-04 10:27:46 -0800

    refractor code and separate front end and back end on the weather js code

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -25,16 +25,13 @@ $(document).ready(function(){
   $('#time').text(moment());
 });
 
-var apiKey = "129327718f475acb9e16c2286c58b85c";
+var Weather = require('./../js/weather.js').weatherModule;
 
 $(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weatherLocation').click(function() {
-    debugger;
     var city = $('#location').val();
-    $('#location').val();
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' +apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
+    $('#location').val("");
+  currentWeatherObject.getWeather(city);
   });
 });
